852d89cd8a1dd7443f6455904a6c03a7d491d474
覃二\DHSYZ <987354940@qq.com>
2019-12-25 14:15:22 +0800

    修改接口

--- src/assets/js/req.js
+++ src/assets/js/req.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const urlBase = 'http://www.zhangqinblog.com/api/Jason_Blog/index.php?' // 线上环境
+const urlBase = 'http://www.zhangqinblog.com:8080/API/index.php?' // 线上环境
 // const urlBase = 'http://localhost/newBlog/api/Jason_Blog/index.php?' // 本地环境
 
 // get请求封装
